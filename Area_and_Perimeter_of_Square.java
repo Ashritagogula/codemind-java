dda0fb2ac8a7e5870f2fcfa35cb2832db3282ab3
Ashrita Gogula <ashritagogula12@gmail.com>
2024-08-11 10:09:31 +0530

    import java.util.Scanner; public class Area{     public static void main(String args[]){         Scanner read=new Scanner(System.in);         int a=read.nextInt();         System.out.println(a*a + " " + a*4);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Area{
+    public static void main(String args[]){
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        System.out.println(a*a + " " + a*4);
+    }
+}
